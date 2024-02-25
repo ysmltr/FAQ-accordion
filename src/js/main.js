ee16d9c3ef7d7df4d77e02fbbc6c307df4174f8c
ysmltr <yesimltr1@gmail.com>
2024-02-25 22:35:46 +0300

    Fix formatting in index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
-//* lets start js
+const faqAccordion = document.querySelectorAll(".faq-items");
+
+faqAccordion.forEach((item) => {
+    const faqTitle = item.querySelector(".faq-header");
+    faqTitle.addEventListener("click", () => {
+        const content = item.querySelector(".content");
+        const contentActived = document.querySelector(".active");
+
+        VerifyActive(item, content, contentActived);
+    });
+});
+
+function VerifyActive(item, content, contentActived) {
+    const iconItem = item.querySelector(".plus-icon");
+
+    if (contentActived) {
+        contentActived.style.height = 0;
+        contentActived.classList.remove("active");
+        iconItem.src = "./assets/images/icon-plus.svg";
+    }
+
+    if (content !== contentActived) {
+        iconItem.src = "./assets/images/icon-minus.svg";
+        content.classList.add("active");
+        content.style.height = content.scrollHeight + "px";
+    }
+}
